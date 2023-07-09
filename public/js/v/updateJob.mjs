a76ffe7bad7b73e0fd88b14cfa2100ff3eaf4789
ilia-karasev <133093525+ilia-karasev@users.noreply.github.com>
2023-07-09 21:05:05 +0200

    fix updateJob

--- public/js/v/updateJob.mjs
+++ public/js/v/updateJob.mjs
@@ -9,6 +9,32 @@ const formEl = document.forms["Job"],
   updateButton = formEl["commit"],
   selectJobEl = formEl["selectJob"];
 
+formEl["jobId"].addEventListener("input", function () {
+  // do not yet check the ID constraint, only before commit
+  formEl["jobId"].setCustomValidity(Job.checkJobId(formEl["jobId"].value).message);
+});
+formEl["jobName"].addEventListener("input", function () {
+  formEl["jobName"].setCustomValidity(Job.checkJobName(formEl["jobName"].value).message);
+});
+formEl["location"].addEventListener("input", function () {
+  formEl["location"].setCustomValidity(Job.checkLocation(formEl["location"].value).message);
+});
+formEl["company"].addEventListener("input", function () {
+  formEl["company"].setCustomValidity(Job.checkCompany(formEl["company"].value).message);
+});
+formEl["salary"].addEventListener("input", function () {
+  formEl["salary"].setCustomValidity(Job.checkSalary(formEl["salary"].value).message);
+});
+formEl["typeOfEmployment"].addEventListener("input", function () {
+  formEl["typeOfEmployment"].setCustomValidity(Job.checkTypeOfEmployment(formEl["typeOfEmployment"].value).message);
+});
+formEl["jobFieldCategory"].addEventListener("input", function () {
+  formEl["jobFieldCategory"].setCustomValidity(Job.checkJobFieldCategory(formEl["jobFieldCategory"].value).message);
+});
+formEl["description"].addEventListener("input", function () {
+  formEl["description"].setCustomValidity(Job.checkDescription(formEl["description"].value).message);
+});
+
 // Set up select element
 // fill select with options
 for (const jobRec of jobRecords) {
@@ -24,6 +50,12 @@ selectJobEl.addEventListener("change", async function () {
   if (jobId) {
     // retrieve up-to-date job record
     const jobRec = await Job.retrieve(jobId);
+    for (const field of ["jobId", "jobName", "location", "company", 
+    "salary", "typeOfEmployment", "jobFieldCategory", "description"]) {
+      formEl[field].value = jobRec[field] !== undefined ? jobRec[field] : "";
+      // delete custom validation error message which may have been set before
+      formEl[field].setCustomValidity("");
+    }
     formEl["jobId"].value = jobRec.jobId;
     formEl["jobName"].value = jobRec.jobName;
     formEl["location"].value = jobRec.location;
@@ -52,8 +84,45 @@ updateButton.addEventListener("click", async function () {
   },
   jobIdRef = selectJobEl.value;
   if (!jobIdRef) return;
-  await Job.update(slots);
-  // update the selection list option element
-  selectJobEl.options[selectJobEl.selectedIndex].text = `${slots.jobName} at ${slots.company} (${slots.location}) - ${slots.jobFieldCategory}`;
-  formEl.reset();
+   // set error messages in case of constraint violations
+   formEl["jobName"].addEventListener("input", function () {
+    formEl["jobName"].setCustomValidity(
+      Job.checkJobName(slots.jobName).message);
+  });
+  formEl["location"].addEventListener("input", function () {
+    formEl["location"].setCustomValidity(
+      Job.checkLocation(slots.location).message);
+  });
+  formEl["company"].addEventListener("input", function () {
+    formEl["company"].setCustomValidity(
+      Job.checkCompany(slots.company).message);
+  });
+  formEl["salary"].addEventListener("input", function () {
+    formEl["salary"].setCustomValidity(
+      Job.checkSalary(slots.salary).message);
+  });
+  formEl["typeOfEmployment"].addEventListener("input", function () {
+    formEl["typeOfEmployment"].setCustomValidity(
+      Job.checkTypeOfEmployment(slots.typeOfEmployment).message);
+  });
+  formEl["jobFieldCategory"].addEventListener("input", function () {
+    formEl["jobFieldCategory"].setCustomValidity(
+      Job.checkJobFieldCategory(slots.jobFieldCategory).message);
+  });
+  formEl["description"].addEventListener("input", function () {
+    formEl["description"].setCustomValidity(
+      Job.checkDescription(slots.description).message);
+  });
+
+  if (formEl.checkValidity()) {
+    Job.update(slots);
+    // update the selection list option
+    selectJobEl.options[selectJobEl.selectedIndex].text = slots.jobName;
+    formEl.reset();
+  }
+});
+
+// neutralize the submit event
+formEl.addEventListener("submit", function (e) {
+  e.preventDefault();
 });
